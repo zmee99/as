604b4aaba189ff7b2100de3a8877e1d2d08b4412
zmee99 <131580626+zmee99@users.noreply.github.com>
2023-04-24 21:21:32 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,73 @@
-# as
+# Music-bot by Shuruhatik & niro
+
+All Copyright Go's To Shuruhatik
+
+اذا تريد استخدام بروجكت برجاء معرفة عن البروجكت قانوني ومسجل كل حقوق الخاص بيه
+
+### ✨ Installation
+
+go to `config` folder and edit bot.json file
+
+```js
+{
+    "game": "GAME",//your bot status
+    "prefix": "PREFIX",//your boy prerix 
+    "token_bot": "TOKEN"//your bot token
+}
+```
+
+تذكير :
+
+- `game`, the status of the bot.
+- `prefix`, the prefix that will be set to use the bot.
+- `token_bot`, the token of the bot available on the [Discord Developers](https://discordapp.com/developers/applications) section.
+
+لتخصيص الرموز التعبيرية ، انتقل إلى الملف `emojis.json`.
+يتم تعريف Emojis بالفعل افتراضيًا ولكن يمكنك تعديلها إذا كنت ترغب في ذلك.
+
+
+```js
+{
+    "music": ":musical_note:",
+    "queue": ":bar_chart:",
+    "error": ":x:",
+    "success": ":white_check_mark:"
+}
+```
+
+In the console, type `npm install` to install all dependencies.
+
+To start the bot :
+
+```
+#With Node
+node index.js
+
+#With pm2
+pm2 start index.js --name "MusicBot"
+```
+
+### 🎵 Music commands
+
+```
+play <name>, تشغيل الموسيقى في قناة صوتية.
+pause, وقفة الموسيقى الحالية.
+resume, يعيد تشغيل الموسيقى الحالية. 
+queue, انظر الموسيقى القادمة.
+clear-queue, احذف الموسيقى التالية.
+shuffle, لخلط قائمة الانتظار.
+np,انظر الموسيقى قيد التقدم.
+loop,لتمكين أو تعطيل وظيفة التكرار.
+volume <1 - 100>, لتغير مستوي الصوت
+skip, انتقل إلى الموسيقى التالية.
+stop, أوقف كل الموسيقى.
+
+```
+
+### 🛠️ Support
+
+Server Support : [Discord](https://dsc.gg/shuruhatik)
+
+Website : [Click Here to Go](https://www.shuruhatik.ml/)
+
+You Can Call Me In Discord : Shuruhatik#0001 
---
+++ index.js
@@ -0,0 +1,68 @@
+const express = require("express");
+const app = express();
+ 
+app.listen(() => console.log(""));
+ 
+app.use('/ping', (req, res) => {
+  res.send(new Date());
+});
+
+const fs = require('fs');
+const discord = require('discord.js');
+
+const client = new discord.Client({ disableMentions: 'everyone' });
+
+const { Player } = require('discord-player');
+
+const player = new Player(client);
+client.player = player;
+client.config = require('./config/bot.json');
+client.emotes = require('./config/emojis.json');
+client.filters = require('./config/filters.json');
+client.commands = new discord.Collection();
+
+setInterval(() => {
+  const channel = client.channels.cache.get(client.config.channel);
+  if (!channel) return console.error("I can't find this channel!");
+  channel
+    .join()
+    .then(con => {
+      console.log("Working!");
+    })
+    .catch(e => {
+      console.error(e);
+    });
+}, 3000);
+
+fs.readdir('./events/', (err, files) => {
+    if (err) return console.error(err);
+    files.forEach(file => {
+        const event = require(`./events/${file}`);
+        let eventName = file.split(".")[0];
+        console.log(`Loading event ${eventName} 🌚`);
+        client.on(eventName, event.bind(null, client));
+    });
+});
+
+fs.readdir('./player-events/', (err, files) => {
+    if (err) return console.error(err);
+    files.forEach(file => {
+        const event = require(`./player-events/${file}`);
+        let eventName = file.split(".")[0];
+        console.log(`Loading player event ${eventName} 🌚`);
+        client.player.on(eventName, event.bind(null, client));
+    });
+});
+
+fs.readdir('./commands/', (err, files) => {
+    if (err) return console.error(err);
+    files.forEach(file => {
+        if (!file.endsWith(".js")) return;
+        let props = require(`./commands/${file}`);
+        let commandName = file.split(".")[0];
+        console.log(`Loading command ${commandName} 🌚`);
+        client.commands.set(commandName, props);
+    });
+});
+
+client.login(client.config.token_bot);
